6c95a2bfc38f9113bce9b99037e6124034b48c26
NEEL PRAJAPATI <83747762+catalog2003@users.noreply.github.com>
2024-03-24 14:07:07 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ import threading
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/convert": {"origins": "http://127.0.0.1:5500"}})
 
 conversion_threads = {}
 
