52d677b6a38fcca5bfa349a3b961ba7363e14f06
NEEL PRAJAPATI <83747762+catalog2003@users.noreply.github.com>
2024-03-24 16:08:53 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -32,7 +32,15 @@ def index():
 
 @app.route('/stream')
 def stream():
-    return Response(stream_with_context(generate_numbers()), content_type='text/html')
+    def generate():
+        count = 1
+        while True:
+            yield f"data: {count}\n\n"
+            count += 1
+            time.sleep(1)
+
+    return Response(generate(), mimetype="text/event-stream")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
