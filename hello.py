caf835faefe19f4ac8278191f2e6421451c36b63
NEEL PRAJAPATI <83747762+catalog2003@users.noreply.github.com>
2024-03-24 15:48:24 +0530

    j

--- hello.py
+++ hello.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+import threading
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello"
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-if __name__ == "__main__":
+@app.route('/start_infinite', methods=['POST'])
+def start_infinite():
+    def infinite_response():
+        while True:
+            yield '1\n'
+
+    return app.response_class(infinite_response(), mimetype='text/plain')
+
+if __name__ == '__main__':
     app.run(debug=True)
